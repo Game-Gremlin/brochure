6784593568ac5d783a5262a894952da4e21c94f9
ryanpf <ryanpf@users.noreply.github.com>
2022-04-15 21:27:30 +0000

    deploy: f17167cf1af824c90c1e2107cef09f6faea58481

---
+++ _next/static/APLJO-HrcSvWxFMtwNeSM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4b816f88d5d9fbd9.css","static/chunks/pages/index-8df4bbd3b0218398.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
